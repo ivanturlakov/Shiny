758cb2c2137d0b9f36e02788de09f793ffd795f9
Ivan <turlakov84@gmail.com>
2019-04-25 16:14:04 +0300

    first commit

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	  
+	  
+	  //Nav
+	  $( ".nav-togle" ).click(function() {
+	      $( "#nav" ).slideToggle( "slow" );
+	  });
+	  
+	  //More
+	  $( ".more" ).click(function() {
+	      $( ".more_desc" ).slideToggle( "slow" );
+	  });
+
+});
